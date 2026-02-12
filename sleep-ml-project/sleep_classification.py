58fbe43d31f02001aca89ad8013cd7500274a438
Jason Lin <Jasonlin1079@gmail.com>
2026-02-12 16:30:28 -0500

    add basic data output and IQR test

--- sleep-ml-project/sleep_classification.py
+++ sleep-ml-project/sleep_classification.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import kagglehub
@@ -14,3 +15,33 @@ df = pd.read_csv(csv_file)
 
 print("Dataset Shape:", df.shape)
 print(f"Rows: {df.shape[0]}, Columns: {df.shape[1]}\n")
+
+#Step 4: Display columnn names and their data types
+print("Column Names and Data Types:")
+print(df.dtypes)
+print("\n")
+
+print("First 5 Rows:")
+print(df.head())
+print("\n")
+
+print("Dataset Info")
+df.info()
+print("\n")
+
+print("Statistical Summary")
+print(df.describe())
+
+#Outlier detection (IQR method)
+Q1 = df.select_dtypes(include='number').quantile(0.25)
+Q3 = df.select_dtypes(include='number').quantile(0.75)
+IQR = Q3 - Q1
+
+numeric_df = df.select_dtypes(include='number')
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+outliers = ((numeric_df < lower_bound) | (numeric_df > upper_bound)).sum()
+
+print("Outlier counts per columne :\n", outliers)
